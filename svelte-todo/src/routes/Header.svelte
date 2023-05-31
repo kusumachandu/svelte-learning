0dac5d7d775b9ecf9274da775c9b8358a2cd6798
chandrashekar <kusumachandu43@gmail.com>
2023-05-31 15:45:53 +0530

    todo app svelte

---
+++ svelte-todo/src/routes/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	<h1>
+		TO-DO's
+	</h1>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+	}
+</style>
