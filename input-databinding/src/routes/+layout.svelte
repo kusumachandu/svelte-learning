cfade8657a54b7c462e9773ba3276c5b141b5f2e
chandrashekar <kusumachandu43@gmail.com>
2023-06-01 12:04:29 +0530

    input & data-binding practice

---
+++ input-databinding/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app h-screen">
+	<header class="font-bold uppercase text-center py-10">
+		<p>Input and data-binding</p>
+	</header>
+	<main class=" w-full">
+		<slot />
+	</main>
+</div>
---
+++ input-databinding/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts,tsx,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
